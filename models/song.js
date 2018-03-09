71c5ffdb7cf8259aabedd0d64ce985628cb7cc43
David de Jhesus Vargas Banda <xoliu33@gmail.com>
2018-03-09 14:41:31 -0600

    second commit

---
+++ models/song.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var SongSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    _artist: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Artist'
+    },
+    _album: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Album'
+    },
+    duration: {
+        type: Number,
+    }
+
+});
+
+module.exports = mongoose.model('Song', SongSchema);
